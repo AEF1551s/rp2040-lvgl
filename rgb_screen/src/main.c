c38ecc3aa8d9de4764548cd3d0694abd232d231b
Krisjanis Ivbulis <ivbulisk@gmail.com>
2025-07-21 13:10:38 +0300

    remove manual refresh

--- rgb_screen/src/main.c
+++ rgb_screen/src/main.c
@@ -4,18 +4,20 @@
 #include "pico/stdlib.h"
 
 #include "turtle.h"
+#include "motor.h"
 
 #include "lv_screen.h"
 
+#include "time.h"
 int main()
 {
     stdio_init_all();
     screen_init();
     turtle_init();
-
+    motor_init();
+    
     while (true)
     {
         lv_timer_handler(); // Required for LVGL to refresh GUI
-        update_label_number(get_turtle());
     }
 }
