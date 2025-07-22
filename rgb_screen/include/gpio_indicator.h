5ff658833594d40571a33387ef96375a97efd803
Krisjanis Ivbulis <ivbulisk@gmail.com>
2025-07-22 21:42:56 +0300

    add placeholder hedare for gpio indicators

---
+++ rgb_screen/include/gpio_indicator.h
@@ -0,0 +1,11 @@
+#if !defined(GPIO_INDICATOR_H)
+#define GPIO_INDICATOR_H
+
+#include "lv_conf.h"
+#include "lvgl.h"
+#include "lv_port_disp.h"
+
+extern bool indicator_left_flag;
+extern bool indicator_right_flag;
+
+#endif // GPIO_INDICATOR_H
---
+++ rgb_screen/src/gpio_indicator.c
@@ -0,0 +1,5 @@
+#include "gpio_indicator.h"
+
+bool indicator_left_flag = false;
+bool indicator_right_flag = false;
+//  TODO: implement gpio irq for button press
