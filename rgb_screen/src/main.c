bd27a3042846ce5ddc9c7091fbf73cb8e524dbe5
Krisjanis Ivbulis <ivbulisk@gmail.com>
2025-07-22 21:43:15 +0300

    add indicator flash flag check

--- rgb_screen/src/main.c
+++ rgb_screen/src/main.c
@@ -5,6 +5,8 @@
 
 #include "turtle.h"
 #include "motor.h"
+#include "indicators.h"
+#include "gpio_indicator.h"
 
 #include "lv_screen.h"
 
@@ -15,13 +17,22 @@ int main()
     screen_init();
     turtle_init();
     motor_init();
-    
+
     while (true)
     {
         lv_timer_handler(); // Required for LVGL to refresh GUI
-        
-        if(filter_needed){
+
+        if (filter_needed)
+        {
             filter_turtle();
+        }
+        if (indicator_right_flag)
+        {
+            indicator_timer_init(true);
+        }
+        if (indicator_left_flag)
+        {
+            indicator_timer_init(false);
         }
     }
 }
